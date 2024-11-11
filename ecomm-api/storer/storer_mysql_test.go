87a97eb29a597299e136d530dd41da9475f709a8
rileyarnie <arnold.ril3y@gmail.com>
2024-11-11 16:05:47 +0300

    add test cases

--- ecomm-api/storer/storer_mysql_test.go
+++ ecomm-api/storer/storer_mysql_test.go
@@ -87,56 +87,61 @@ func TestCreateProduct(t *testing.T) {
 
 }
 
-func TestGetProduct(t *testing.T) {
+func TestListProducts(t *testing.T) {
 	p := &Product{
 		Name:         "test product",
-		Image:        "test image",
+		Image:        "test.jpg",
 		Category:     "test category",
 		Description:  "test description",
 		Rating:       5,
-		NumReviews:   10,
-		Price:        100.0,
-		CountInStock: 100,
+		NumReviews:   100,
+		Price:        99.99,
+		CountInStock: 10,
 	}
 
-	testCases := []struct {
+	tcs := []struct {
 		name string
 		test func(*testing.T, *MySQLStorer, sqlmock.Sqlmock)
 	}{
 		{
 			name: "success",
 			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"id", "name", "image", "category", "description", "rating", "num_reviews", "price", "count_in_stock", "created_at", "updated_at"}).
+					AddRow(1, p.Name, p.Image, p.Category, p.Description, p.Rating, p.NumReviews, p.Price, p.CountInStock, p.CreatedAt, p.UpdatedAt)
+				mock.ExpectQuery("SELECT * FROM products").WillReturnRows(rows)
 
-				rows := sqlmock.NewRows([]string{"id", "name", "image", "category", "description", "rating", "num_reviews", "price", "count_in_stock", "created_at", "updated_at"}).AddRow(1, p.Name, p.Image, p.Category, p.Description, p.Rating, p.NumReviews, p.Price, p.CountInStock, p.CreatedAt, p.UpdatedAt)
-
-				mock.ExpectQuery("SELECT * FROM products WHERE id=?").WithArgs(1).WillReturnRows(rows)
-
-				gp, err := st.GetProduct(context.Background(), 1)
+				products, err := st.ListProducts(context.Background())
 				require.NoError(t, err)
-				require.Equal(t, int64(1), gp.ID)
+				require.Len(t, products, 1)
 
 				err = mock.ExpectationsWereMet()
 				require.NoError(t, err)
-			}},
+			},
+		},
 		{
-			name: "failed getting product",
+			name: "failed querying products",
 			test: func(t *testing.T, st *MySQLStorer, mock sqlmock.Sqlmock) {
-				mock.ExpectQuery("SELECT * FROM products WHERE id=?").WithArgs(1).WillReturnError((fmt.Errorf("error getting product")))
-				_, err := st.GetProduct(context.Background(), 1)
+				mock.ExpectQuery("SELECT * FROM products").WillReturnError(fmt.Errorf("error querying products"))
+
+				_, err := st.ListProducts(context.Background())
 				require.Error(t, err)
+
 				err = mock.ExpectationsWereMet()
 				require.NoError(t, err)
-
-			}},
+			},
+		},
 	}
 
-	for _, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
-			withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
-				st := NewMySQLStorer(db)
-				testCase.test(t, st, mock)
-
-			})
+	for _, tc := range tcs {
+		withTestDB(t, func(db *sqlx.DB, mock sqlmock.Sqlmock) {
+			st := NewMySQLStorer(db)
+			tc.test(t, st, mock)
 		})
 	}
+}
+
+func TestUpdateProduct(t *testing.T) {
+}
+
+func TestDeleteProduct(t *testing.T) {
 }
